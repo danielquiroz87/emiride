feb9a37dd4c81b0494c502994efcc43b41104899
tito <juffigueroama@unal.edu.co>
2023-05-26 10:50:17 -0500

    added the sitemap.xml now will add the picanto

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense } from 'react';
 import {Routes, Route, Navigate} from 'react-router-dom'; //Switch changed to routes Also redirect is changed to Navigate since version 6
 
 
 import HomeEn from './home-components/EN/HomeComponentEn';
 import AboutUs from './home-components/EN/aboutComponent';
-import PrivacyPolicy from './home-components/EN/PrivacyPolicy';
-import TermsConditions from './home-components/EN/TermConditions';
-
 import HomeEs from './home-components/ES/HomeComponentEs';
 import SobreNosotros from './home-components/ES/aboutComponent_ES';
-import PoliticaPrivacidad from './home-components/ES/PrivacyPolicy_ES';
-import TermsConditions_ES from './home-components/ES/TermsConditions_ES';
+
+// import as here https://web.dev/code-splitting-suspense/
+
+const TermsConditions = lazy(() => import('./home-components/EN/TermConditions'));
+const TermsConditionsComponent = () => (
+  <Suspense fallback={renderLoader()}>
+    <TermsConditions/>
+  </Suspense>
+)
+
+const PrivacyPolicy = lazy(() => import('./home-components/EN/PrivacyPolicy'));
+const PrivacyPolicyComponent = () => (
+  <Suspense fallback={renderLoader()}>
+    <PrivacyPolicy/>
+  </Suspense>
+)
+
+const TermsConditionsEs = lazy(() => import('./home-components/ES/TermsConditions_ES'));
+const TermsConditionsEsComponent = () => (
+  <Suspense fallback={renderLoader()}>
+    <TermsConditionsEs/>
+  </Suspense>
+)
+
+const PoliticaPrivacidad = lazy(() => import('./home-components/ES/PrivacyPolicy_ES'));
+const PoliticaPrivacidadComponent = () => (
+  <Suspense fallback={renderLoader()}>
+    <PoliticaPrivacidad/>
+  </Suspense>
+)
+
+// message for loading component
+const renderLoader = () => <p>Loading</p>;
 
 function Main(){
    
-
 
 //   useEffect(() => {
 
@@ -27,13 +54,13 @@ function Main(){
         <Routes>
             <Route path = "/"               element={<HomeEn/>}/>
             <Route path = "/about-us"       element={<AboutUs/>}/>
-            <Route path = "/privacy-policy" element={<PrivacyPolicy/>} />
-            <Route path = "/terms-conditions" element={<TermsConditions/>} />
+            <Route path = "/privacy-policy" element={<PrivacyPolicyComponent/>} />
+            <Route path = "/terms-conditions" element={<TermsConditionsComponent/>} />
 
             <Route path = "/inicio"         element={<HomeEs/>}/> 
             <Route path = '/sobre-nosotros' element={<SobreNosotros/>}/>
-            <Route path = '/politica-privacidad' element={<PoliticaPrivacidad/>}/>
-            <Route path = "/terminos-condiciones" element={<TermsConditions_ES/>} />
+            <Route path = '/politica-privacidad' element={<PoliticaPrivacidadComponent/>}/>
+            <Route path = "/terminos-condiciones" element={<TermsConditionsEsComponent/>} />
 
             <Route path = "*"               element={<Navigate to="/" />} />
             {/* Instead of redirect the above is needed to redirect if there is no matched url*/}
